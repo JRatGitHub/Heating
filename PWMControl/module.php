4a1c4018ccb73eede133a2e03622aef135c68416
JRatGitHub <janroelof@dewit-qian.nl>
2021-01-23 22:47:53 +0100

    Update module.php

--- PWMControl/module.php
+++ PWMControl/module.php
@@ -15,6 +15,9 @@
 			 $PWMSetpoint = $this->RegisterVariableInteger('PWMSetpoint', 'Setpoint', '~Intensity.100',0);
 			 $PWMOutput = $this->RegisterVariableBoolean('PWMOutput', 'Output');
 
+			 //Timers
+			$this->RegisterTimer('OpenTimer', 0, "VALVE_CalculatePWM(\$_IPS['TARGET']);");
+			$this->RegisterTimer('ClosedTimer', 0, "VALVE_CalculatePWM(\$_IPS['TARGET']);");
 		}
 
 		public function Destroy()
@@ -33,19 +36,29 @@
 		}
 
 		public function MessageSink($TimeStamp, $SenderID, $Message, $Data) {
-			IPS_LogMessage("PWMControl", "Message from SenderID ".$SenderID." with Message ".$Message."\r\n Data: ".print_r($Data, true));
+		//	IPS_LogMessage("PWMControl", "Message from SenderID ".$SenderID." with Message ".$Message."\r\n Data: ".print_r($Data, true));
 			if ($Message == VM_UPDATE) {
 				//IPS_LogMessage("PWMControl:MessageSink", "Updated");
 				if ($SenderID == $this->GetIDForIdent('PWMSetpoint')){
 					IPS_LogMessage("PWMControl:MessageSink", "PWMSetpoint Updated");
 					IPS_LogMessage("PWMControl:MessageSink", "PWMSetpoint old value:".$Data[2] ." %");
 					IPS_LogMessage("PWMControl:MessageSink", "PWMSetpoint new value:".$Data[0] ." %");
+
+
 				}
 
 			}
 		}
 
+
+		protected function CalculatePWM($Setpoint){
+			IPS_LogMessage("PWMControl", "CalculatePWM triggered with setpoint: ".$Setpoint);
+		}
 		protected function SetPWM($Setpoint){
 			IPS_LogMessage("PWMControl", "SetPWM triggered with setpoint: ".$Setpoint);
+			$duration = $this->ReadPropertyInteger('CycleTime') * $Setpoint;
+			IPS_LogMessage("PWMControl", "SetPWM duration: ".$duration . "Sec.");
+			//$this->SetTimerInterval('OpenTimer', $duration * 1000);
+
 		}
 	}
