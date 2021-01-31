f9cd74da73c2461282baa38f9f16b47f7cf1e10c
JRatGitHub <janroelof@dewit-qian.nl>
2021-01-31 13:03:06 +0100

    Update module.php

--- PIDControl/module.php
+++ PIDControl/module.php
@@ -16,9 +16,9 @@
 			 $this->RegisterPropertyInteger('OutputMin',10);
 			 $this->RegisterPropertyInteger('OutputMax',100);
 			
-			$CatID = IPS_CreateCategory();
-			IPS_SetName($CatID, "CategoryDuringCreate");
-			IPS_SetParent($CatID, $this->InstanceID);
+			$CatID = IPS_CreateCategory('ViaFunction');
+			//IPS_SetName($CatID, "CategoryDuringCreate");
+			//IPS_SetParent($CatID, $this->InstanceID);
 		}
 
 		public function Destroy()
@@ -37,11 +37,30 @@
 			IPS_SetParent($CatID, $this->InstanceID);
 		}
 
-		public function CreateCategory($CatName){
-			$CatID = IPS_CreateCategory();
-			IPS_SetName($CatID, "CreateCategory");
-			IPS_SetParent($CatID, $this->InstanceID);
-			
+		public function CreateCategory($Ident){
+			$eid = @$this->GetIDForIdent($Ident);
+			if($eid === false) {
+		    	$eid = 0;
+			} 
+			//elseif(IPS_GetEvent($eid)['EventType'] <> $Typ) {
+		    //	IPS_DeleteEvent($eid);
+		    //	$eid = 0;
+			//}
+			//we need to create one
+			if ($eid == 0) {
+				$CatID = IPS_CreateCategory();
+				IPS_SetName($CatID, "CreateCategory");
+				IPS_SetParent($CatID, $this->InstanceID);	
+				//$EventID = IPS_CreateEvent($Typ);
+		    	//IPS_SetParent($EventID, $Parent);
+		    	//IPS_SetIdent($EventID, $Ident);
+		    	//IPS_SetName($EventID, $Name);
+		    	//IPS_SetPosition($EventID, $Position);
+		    	//IPS_SetEventActive($EventID, true);  
+			}
+		//	$CatID = IPS_CreateCategory();
+		//	IPS_SetName($CatID, "CreateCategory");
+		//	IPS_SetParent($CatID, $this->InstanceID);	
 		}
 
 	}
