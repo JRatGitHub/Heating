ddc4f3e1d5909c3793a7009a301150fb0d76ef98
JRatGitHub <janroelof@dewit-qian.nl>
2021-10-18 18:25:25 +0200

    Update module.php

--- PWMControl/module.php
+++ PWMControl/module.php
@@ -28,7 +28,7 @@
 
 			//intialize
 			SetValueBoolean($this->GetIDForIdent('PWMOutput'),False);
-			$this->SetValue('Status', 'Initialized');
+			SetValueString($this->GetIDForIdent('Status'), 'Initialized');
 			//SetValueBoolean($this->ReadPropertyInteger('ValveID'),False);
 		}
 
